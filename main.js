3b042ef31af885de6edcb9c4c5fab58cd28f7466
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-12 13:11:18 -0600

    helpful changes which enabled the live output of the range slider

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+
 const billTotal = document.getElementById('bill-input');
 const tipPercentage = document.getElementById('service-input');
-let percentageText = document.getElementById('symbol').innerHTML;
+// let percentageText = document.getElementById('symbol').innerHTML;
 // console.log(percentageText);
 
 tipPercentage.addEventListener("input", function(value) {
-    let tipPercentValue = tipPercentage.value;
-    percentageText.replace('0%', tipPercentValue);
-    console.log(tipPercentValue);
-    console.log(percentageText);
+    // let tipPercentValue = tipPercentage.value;
+    document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
+    // console.log(tipPercentValue);
+    // console.log(percentageText);
 });
