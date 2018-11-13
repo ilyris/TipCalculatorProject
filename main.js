b61af11bee293a3bfc51fb0c61ef2047bfc94e82
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-13 00:29:17 -0600

    nothing usefull

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-
 const billTotal = document.getElementById('bill-input');
 const tipPercentage = document.getElementById('service-input');
 const typedPercentage = document.getElementById('typed-percentage-input');
 let total;
 
 
-tipPercentage.addEventListener("input", function() {
+ tipPercentage.addEventListener("input", function() {
     document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
-    total = billTotal.value * tipPercentage.value / 100;
-    if( billTotal.value <= 0)     {
+     total = billTotal.value * tipPercentage.value / 100;
+     if( billTotal.value <= 0)     {
         document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
     } else if(total >= 0) {
         document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
@@ -24,4 +23,3 @@ typedPercentage.addEventListener('keyup', function() {
         document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
     }
 });
-
