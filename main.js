b510a185e5c1bf0dd2d884500dad506425f54299
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-12 20:49:24 -0600

    Tip calculator is working. Changes included small styling and functionaity with the slider calculating the tip

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 
 const billTotal = document.getElementById('bill-input');
 const tipPercentage = document.getElementById('service-input');
+const  typedPercentage = document.getElementById('label-for-typed-percentage');
 // let percentageText = document.getElementById('symbol').innerHTML;
 // console.log(percentageText);
+let total;
 
-tipPercentage.addEventListener("input", function(value) {
-    // let tipPercentValue = tipPercentage.value;
+
+tipPercentage.addEventListener("input", function() {
     document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
-    // console.log(tipPercentValue);
-    // console.log(percentageText);
+
+    total = billTotal.value * tipPercentage.value / 100;
+
+    if( billTotal.value <= 0)     {
+        document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
+    } else if(total >= 0) {
+        document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
+    }
 });
