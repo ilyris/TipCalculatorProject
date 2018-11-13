f8e3d0246addc67b2a0e6bef43749026efe4ed4f
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-12 23:10:39 -0600

    range input and text input are outputting the correct Tip Ammount into the DOM

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 
 const billTotal = document.getElementById('bill-input');
 const tipPercentage = document.getElementById('service-input');
-const  typedPercentage = document.getElementById('label-for-typed-percentage');
-// let percentageText = document.getElementById('symbol').innerHTML;
-// console.log(percentageText);
+const typedPercentage = document.getElementById('typed-percentage-input');
 let total;
 
 
 tipPercentage.addEventListener("input", function() {
     document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
-
     total = billTotal.value * tipPercentage.value / 100;
-
     if( billTotal.value <= 0)     {
         document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
     } else if(total >= 0) {
         document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
     }
 });
+   
+typedPercentage.addEventListener('keyup', function() {
+    document.getElementById('symbol').innerHTML = typedPercentage.value + "%";
+    total = billTotal.value * typedPercentage.value / 100;
+    if( billTotal.value <= 0)     {
+        document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
+    } else if(total >= 0) {
+        document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
+    }
+});
+
