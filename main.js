ad03b5abfeb9951be2d238324960b1f08c338ff8
Dylan Sieren <dylan.sieren01@gmail.com>
2018-11-17 22:52:15 -0600

    refactored code, cleaned up the conditional statements, and debugged the party member functionality

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
 const billTotal = document.getElementById('bill-input');
 const tipPercentage = document.getElementById('service-input');
 const typedPercentage = document.getElementById('typed-percentage-input');
-let numberOfPartyMembers;
-let total;
+let numberOfPartyMembers, total, splitTotal;
+
+function calculateTip() {
+    numberOfPartyMembers = document.getElementById('party-number').value;      
+    document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
+    total = billTotal.value * tipPercentage.value / 100;
+    splitTotal = total / numberOfPartyMembers;
+    if(numberOfPartyMembers > 1 && total >= 0) {
+        document.querySelector('.total-tip').innerHTML = "Your tip per person is: $" + splitTotal.toFixed(2); 
+    }   
+    if( billTotal.value <= 0)     {
+        document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
+    }   else if(total > 0 && numberOfPartyMembers == 1) {
+        document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
+    }
+};
 
 tipPercentage.addEventListener("input", () => {
     typedPercentage.value = tipPercentage.value;
-    numberOfPartyMembers = document.getElementById('party-number').value;    
-    document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
-    total = billTotal.value * tipPercentage.value / 100;
-    total = total / numberOfPartyMembers;
-        if(numberOfPartyMembers > 1 && total >= 0) {
-            total = total / numberOfPartyMembers;
-            document.querySelector('.total-tip').innerHTML = "Your tip per person is: $" + total.toFixed(2); 
-        }   
-        if( billTotal.value <= 0)     {
-            document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
-        }  else if(total >= 0 ) {
-            document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
-        }
+    calculateTip();
 });
 
 
 typedPercentage.addEventListener("input", () => {
     tipPercentage.value = typedPercentage.value; 
-    numberOfPartyMembers = document.getElementById('party-number').value;    
-    document.getElementById('symbol').innerHTML = tipPercentage.value + "%";
-    total = billTotal.value * tipPercentage.value / 100;
-    total = total / numberOfPartyMembers;
-        if(numberOfPartyMembers > 1 && total >= 0) {
-            total = total / numberOfPartyMembers;
-            document.querySelector('.total-tip').innerHTML = "Your tip per person is: $" + total.toFixed(2);
-        }   
-        if(billTotal.value <= 0)     {
-            document.querySelector('.total-tip').innerHTML = "I can't serve my purpose unless there is a bill total, sorry";
-        } else if(total >= 0) {
-            document.querySelector('.total-tip').innerHTML = "Your tip is: $" + total.toFixed(2); 
-        }
+    calculateTip();
 });
-console.log(total);
